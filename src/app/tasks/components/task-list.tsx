5417a4c94311d484fbcd617e838f5fc11637835c
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-08-27 15:11:07 +0100

    feat: implement TaskList

---
+++ src/app/tasks/components/task-list.tsx
@@ -0,0 +1,27 @@
+/*--------------------------------------------------------------------------------------------------
+ *                       Copyright (c) Ayyoub EL Kouri. All rights reserved
+ *     Becoming an expert won’t happen overnight, but with a bit of patience, you’ll get there
+ *------------------------------------------------------------------------------------------------*/
+
+import { useTasksState } from '../store/use-tasks-store';
+import type { Task } from '../types/entities';
+import TaskItem from './task-item';
+
+export default function TaskList() {
+   const { find } = useTasksState();
+   const tasks = find();
+
+   return (
+      <div
+         className='w-[1043px] h-[524px] bg-[#DADADA] rounded-[16px] border-1 border-black/30
+                  dark:border-white/10 dark:bg-[#090808] py-[35px] pl-[35px] pr-[17px]
+                    overflow-auto'
+      >
+         <div className='task-list pr-[17px] w-full h-full flex flex-col gap-3 overflow-auto'>
+            {tasks.map((task: Task) => (
+               <TaskItem key={task.id} task={task} />
+            ))}
+         </div>
+      </div>
+   );
+}
