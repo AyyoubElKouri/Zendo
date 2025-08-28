264a3af2e78608938fd64efda0a4ac7631e068ac
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-08-28 14:14:08 +0100

    feat: migrate TaskItem to use TaskRepository class

---
+++ src/app/tasks/components/TaskItem.tsx
@@ -0,0 +1,90 @@
+/*--------------------------------------------------------------------------------------------------
+ *                       Copyright (c) Ayyoub EL Kouri. All rights reserved
+ *     Becoming an expert won’t happen overnight, but with a bit of patience, you’ll get there
+ *------------------------------------------------------------------------------------------------*/
+
+'use client';
+
+import clsx from 'clsx';
+import Image from 'next/image';
+import { useMemo, type ReactNode } from 'react';
+
+import { Button } from '@/components/ui/button';
+
+import type { Task } from '../types/entities';
+import TaskField from './task-field';
+import TaskRepositoryImpl from '../services/persistence/TaskRepositoryImpl';
+import useTaskItemAdapter from '../hooks/useTaskItemAdapter';
+
+export default function TaskItem({ task }: { task: Task }) {
+   const repository = useMemo(() => new TaskRepositoryImpl(), []);
+   const handlers = useTaskItemAdapter(task, repository);
+
+   return (
+      <TaskItemContainer>
+         <TaskButton type='delete' action={handlers.removeTask} />
+         <TaskButton type='duplicate' action={handlers.duplicateTask} />
+
+         <TaskField task={task} type='source' />
+         <TaskField task={task} type='description' />
+
+         <ToggleButton completed={task.completed} toggleTask={handlers.toggleTask} />
+      </TaskItemContainer>
+   );
+}
+
+/*-------------------------------------- Internel Helpers ----------------------------------------*/
+
+function TaskItemContainer({ children }: { children: ReactNode }) {
+   return (
+      <div
+         className='w-full min-h-[50px] rounded-xl bg-[#F3F3F3] dark:bg-[#050404] border-1
+                  border-black/30 dark:border-white/10 relative grid
+                    grid-cols-[36px_36px_222px_1fr_130px]'
+      >
+         {children}
+      </div>
+   );
+}
+
+interface TaskButtonProps {
+   type: 'delete' | 'duplicate';
+   action: () => void;
+}
+
+function TaskButton({ type, action }: TaskButtonProps) {
+   return (
+      <button
+         type='button'
+         onClick={action}
+         className={clsx(
+            'w-full h-full bg-transparent flex justify-center items-center',
+            'hover:bg-[#e9e9e9] dark:hover:bg-[#292323]',
+            'border-r border-black/30 dark:border-white/10',
+            type === 'delete' && 'hover:rounded-l-xl',
+         )}
+      >
+         <Image
+            src={type === 'delete' ? '/icons/delete.svg' : '/icons/copy.svg'}
+            alt={type === 'delete' ? 'delete icon' : 'copy icon'}
+            width={type === 'delete' ? 25 : 20}
+            height={type === 'delete' ? 25 : 20}
+         />
+      </button>
+   );
+}
+
+interface ToggleButtonProps {
+   completed: boolean;
+   toggleTask: () => void;
+}
+
+function ToggleButton({ completed, toggleTask }: ToggleButtonProps) {
+   return (
+      <div className='w-full h-full flex justify-center items-center p-[10px]'>
+         <Button variant={completed ? 'finished' : 'pending'} onClick={toggleTask}>
+            {completed ? 'Finished' : 'Pending'}
+         </Button>
+      </div>
+   );
+}
