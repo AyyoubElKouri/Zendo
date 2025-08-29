bf4aca0e7dc047e04df3c180443b36c4a01552ce
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-08-29 14:34:17 +0100

    feat: implement ToggleThemeButton component

---
+++ src/features/tasks/components/ToggleThemeButton/ToggleThemeButton.tsx
@@ -0,0 +1,41 @@
+/*--------------------------------------------------------------------------------------------------
+ *                       Copyright (c) Ayyoub EL Kouri. All rights reserved
+ *     Becoming an expert won’t happen overnight, but with a bit of patience, you’ll get there
+ *------------------------------------------------------------------------------------------------*/
+
+'use client';
+
+import clsx from 'clsx';
+import { useTheme } from 'next-themes';
+
+export function ToggleThemeButton() {
+   const { theme, setTheme } = useTheme();
+
+   return (
+      <div
+         className='w-[164px] h-[27px] rounded-full border-1 border-black/30 
+         dark:border-white/10 flex justify-between text-[16px] font-semibold'
+      >
+         <button
+            type='button'
+            onClick={() => setTheme('light')}
+            className={clsx(
+               'w-[82px] border-r-1 border-black/30 dark:border-white/10 rounded-l-full',
+               theme === 'light' ? 'text-black bg-[#b8b8b8]' : 'text-[#CECECE] bg-[#090808]',
+            )}
+         >
+            Light
+         </button>
+         <button
+            type='button'
+            onClick={() => setTheme('dark')}
+            className={clsx(
+               'w-[82px] rounded-r-full',
+               theme === 'light' ? 'text-[#797979] bg-[#F3F3F3]' : 'text-[#FF6200] bg-[#141212]',
+            )}
+         >
+            Dark
+         </button>
+      </div>
+   );
+}
