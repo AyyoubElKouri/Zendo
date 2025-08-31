8b1340406b741b1fd470841874be2b08f1f357a4
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-08-31 22:55:33 +0100

    feat: implement accent-color hook

---
+++ src/types/types.d.ts
@@ -0,0 +1,9 @@
+/*--------------------------------------------------------------------------------------------------
+ *                       Copyright (c) Ayyoub EL Kouri. All rights reserved
+ *     Becoming an expert won’t happen overnight, but with a bit of patience, you’ll get there
+ *------------------------------------------------------------------------------------------------*/
+
+export interface AccentColorType {
+	primary: string;
+	secondary: string;
+}
