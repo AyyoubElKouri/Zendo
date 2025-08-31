7c023c6c24f94ec015e6afef4ea7179d0dfe8c5d
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-08-31 14:12:06 +0100

    refactering 3

--- src/features/tasks/components/Footer/index.ts
+++
@@ -1,6 +0,0 @@
-/*--------------------------------------------------------------------------------------------------
- *                       Copyright (c) Ayyoub EL Kouri. All rights reserved
- *     Becoming an expert won’t happen overnight, but with a bit of patience, you’ll get there
- *------------------------------------------------------------------------------------------------*/
-
-export { Footer } from './Footer';
