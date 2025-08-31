472736c1011a39934472706484bb1c770b6c0079
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-08-31 14:11:10 +0100

    feat: make header use the Toast

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,19 +3,27 @@
  *     Becoming an expert won’t happen overnight, but with a bit of patience, you’ll get there
  *------------------------------------------------------------------------------------------------*/
 
+import { useToast } from '@/hooks/useToast';
+import { Toast } from '@/components/Toast';
+
 export function Header() {
+   const { toast } = useToast();
    const username = 'Ayyoub';
    const date = getFormattedDate();
 
    return (
       <div
-         className='w-full h-16 bg-background-2 border-b-1 border-border pl-4
-                    flex flex-col justify-center items-start'
+         className='w-full h-16 bg-background-2 border-b-1 border-border px-4 flex justify-between
+         items-center'
       >
-         <span className='text-xl font-medium'>
-            Welcome <span className='text-accent'>{username}</span>
-         </span>
-         <span className='text-sm text-secondary'>{date}</span>
+         <div className='flex flex-col justify-center items-start'>
+            <span className='text-xl font-medium'>
+               Welcome <span className='text-accent'>{username}</span>
+            </span>
+            <span className='text-sm text-secondary'>{date}</span>
+         </div>
+
+         {toast.type !== undefined && <Toast toast={toast} />}
       </div>
    );
 }
