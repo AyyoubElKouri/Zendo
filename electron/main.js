3c8f581b0e772683827d69ca7c55a7890a7486af
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-09-02 14:21:02 +0100

    fix: configurations

---
+++ electron/main.js
@@ -0,0 +1,44 @@
+// electron/main.js
+import { app, BrowserWindow, nativeTheme } from "electron";
+import path from "path";
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+function createWindow() {
+	nativeTheme.themeSource = "light";
+	const win = new BrowserWindow({
+		webPreferences: {
+			nodeIntegration: false,
+			contextIsolation: true,
+		},
+		autoHideMenuBar: true,
+		fullscreen: true,
+	});
+	win.webContents;
+
+	// En dev : charge React en mode dev
+	// win.loadURL("http://localhost:5173");
+
+	// En Production.
+
+	win.loadFile(path.join(__dirname, "../dist/index.html"));
+	win.maximize();
+
+	// win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+	createWindow();
+
+	app.on("activate", () => {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
+});
+
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") app.quit();
+});
