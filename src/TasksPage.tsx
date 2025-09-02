6ea2792257da66593f2ba787bb33ec7c936a207b
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-09-02 14:22:04 +0100

    final working version

--- src/TasksPage.tsx
+++ src/TasksPage.tsx
@@ -3,13 +3,26 @@
  *     Becoming an expert won’t happen overnight, but with a bit of patience, you’ll get there
  *------------------------------------------------------------------------------------------------*/
 
+import { ActionBarWithQuote } from "@/components/ActionBarWithQuote";
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+import { Statistics } from "@/components/Statistics";
+import { useLoadTasks } from "./hooks/useLoadTasks";
+import { TaskList } from "@/components/TaskList";
+
 export function TasksPage() {
+	useLoadTasks();
+
 	return (
 		<div
-			className="w-screen h-screen bg-blue-200 text-4xl text-blue-950 flex justify-center
-                    items-center"
+			className="min-w-svh min-h-svh bg-neutral-900 flex flex-col
+                    justify-between items-center "
 		>
-			Hello Ayyoub
+			<Header />
+			<Statistics />
+			<TaskList />
+			<ActionBarWithQuote />
+			<Footer />
 		</div>
 	);
 }
