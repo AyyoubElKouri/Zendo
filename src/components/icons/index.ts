661cd197bcfa13c9c0ec7bc4735f8e2f30775b60
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-09-04 12:02:43 +0100

    feat: seperate icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,10 @@
+/*--------------------------------------------------------------------------------------------------
+ *                       Copyright (c) Ayyoub EL Kouri. All rights reserved
+ *     Becoming an expert won’t happen overnight, but with a bit of patience, you’ll get there
+ *------------------------------------------------------------------------------------------------*/
+
+export { DeleteIcon } from "./DeleteIcon";
+export { DuplicateIcon } from "./DuplicateIcon";
+export { ErrorIcon } from "./ErrorIcon";
+export { QuoteIcon } from "./QuoteIcon";
+export { SuccessIcon } from "./SuccessIcon";
